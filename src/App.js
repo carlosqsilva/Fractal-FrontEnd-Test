3eecf8c3ad2f2e636171a5b38845459ebb6f457b
Carlos Silva <carlosqsilva@outlook.com>
2018-01-30 18:08:26 -0300

    change to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { connect } from "react-redux"
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { HashRouter as Router, Route } from "react-router-dom"
 import styled from "styled-components"
 
 import BeerList from "./components/BeerList"
